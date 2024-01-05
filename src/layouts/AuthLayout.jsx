31206d516980762c672ebbc4705a5a94cc6db419
SnowDevMartin <electronicafr21@gmail.com>
2024-01-05 15:42:21 +0100

    Update - Cambios en home mobile

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -24,7 +24,7 @@ const AuthLayout = () => {
 					<main className='md:w-4/5 lg:w-2/5 xl:2/5 w-full'>
 						{isMobile && iniciarSesion ? (
 							<>
-								<aside className='flex flex-col sticky top-0 max-h-screen min-h-screen gap-10 w-80 md:w-80 lg:w-80 px-10 py-8 mt-20 text-gray-600 font-inter'>
+								<aside className='flex flex-col sticky top-0 gap-10 px-10 py-8 mt-14 text-gray-600 font-inter'>
 									<InfoSidebarAuth
 										setIniciarSesion={setIniciarSesion}
 									/>
